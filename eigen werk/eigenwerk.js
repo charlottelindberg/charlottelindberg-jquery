6c6fc5b4af6d4d67a31e3946225d8619a9901b05
Charlotte Lindberg <charlottelindberg@outlook.com>
2020-04-21 23:12:52 +0200

    Update met folders

---
+++ eigen werk/eigenwerk.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+    console.log("jQuery is ready!");
+});
+
+$(document).ready(function () {
+    $("#clickMe").click(function () {
+    });
+});
+
+$("img").fadeIn(1000);
+$("#picframe").slideToggle("slow");
