7f3ce329f9bc243bceedd9272226faa37779f075
Charlotte Lindberg <charlottelindberg@outlook.com>
2020-04-21 13:20:13 +0200

    Poging02

--- jquery.js
+++ jquery.js
@@ -1,6 +1,25 @@
-$("input[type=text]#insertfield").val(140);
+// $("input[type=text]#insertfield").val(140);
 
 
-$("submit").val(function (i, val) {
-    return value(140)
+// $("submit").val(function (i, val) {
+//     return value(140)
+// });
+
+$("#insertfield").on("focus", function () {
+    $("#insertfield").val("");
+
+    $("#insertfield").on("keyup", function () {
+        var input = $("#insertfield").val();
+        var inputLengte = input.length;
+        console.log("de input is: " + input);
+        console.log("de lengte is: " + inputLengte);
+
+        $("submit.onclick");
+        if (inputLengte >= 140) {
+            alert("OK.");
+        } else {
+            alert("Het bericht bevat meer dan 140 tekens.");
+        }
+    });
 });
+
