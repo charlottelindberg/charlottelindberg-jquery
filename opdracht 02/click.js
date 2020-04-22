257130f311cb31b62d95cdda6bbb647e926b03be
Charlotte Lindberg <charlottelindberg@outlook.com>
2020-04-22 10:47:00 +0200

    Done

--- opdracht 02/click.js
+++ opdracht 02/click.js
@@ -1,1 +1,13 @@
-$('#radio').on('dblclick');
+// $('#radio').on('click', function () {
+//     $(this).css("text-decoration", 'line-through');
+// });
+// $('#radio').on('dblclick', function () {
+//     $(this).fadeOut();
+// })
+
+$('li').on('click', function () {
+    $(this).css("text-decoration", 'line-through');
+});
+$('li').on('dblclick', function () {
+    $(this).fadeOut();
+})
