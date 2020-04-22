7cc670035def87cad2239884b3f9f25722cb886b
Charlotte Lindberg <charlottelindberg@outlook.com>
2020-04-22 10:26:16 +0200

    Done

--- opdracht 01/twitter.js
+++ opdracht 01/twitter.js
@@ -5,21 +5,28 @@
 //     return value(140)
 // });
 
-$("#insertfield").on("focus", function () {
-    $("#insertfield").val("");
+// $("#insertfield").on("focus", function () {
+//     $("#insertfield").val("");
 
-    $("#insertfield").on("keyup", function () {
-        var input = $("#insertfield").val();
-        var inputLengte = input.length;
-        console.log("de input is: " + input);
-        console.log("de lengte is: " + inputLengte);
+//     $("#insertfield").on("keyup", function () {
+//         var input = $("#insertfield").val();
+//         var inputLengte = input.length;
+//         console.log("de input is: " + input);
+//         console.log("de lengte is: " + inputLengte);
 
-        $("#button");
-        if (inputLengte >= 140) {
-            alert("OK.");
-        } else {
-            alert("Het bericht bevat meer dan 140 tekens.");
-        }
-    });
+//         $("#button");
+//         if (inputLengte >= 140) {
+//             alert("OK.");
+//         } else {
+//             alert("Het bericht bevat meer dan 140 tekens.");
+//         }
+//     });
+// });
+
+let counter = 0;
+$("#insertfield").keyup(function () {
+    counter = $(this).val().length;
+    if (counter < 140) {
+        $("#karakters").html(140 - counter + " characters remaining");
+    } else $(this).val() = $(this).val().substring(0, 140);
 });
-
